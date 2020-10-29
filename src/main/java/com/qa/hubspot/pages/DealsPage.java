f4cce1a7961d5dee59ba45bd20449b404e5328d1
Pratiksha Shelke <pratiksha@Pratikshas-MacBook-Pro.local>
2020-10-29 10:27:59 -0400

    made changes in DP class

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,12 +1,22 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.base.BasePage;
 
 public class DealsPage extends BasePage 
 {
-	
-	public DealsPage()
+	By deals = By.id("deals");
+	public DealsPage(WebDriver driver)
 	{
 		System.out.println("Deals Page");
+		driver = this.driver;
+	}
+	
+	public String getTitle()
+	{
+		return driver.getTitle();
+				
 	}
 }
